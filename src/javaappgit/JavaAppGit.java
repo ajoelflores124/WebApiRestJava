b13969afe9a4e28994dc629aedef17675229a112
ANGEL FLORES <ANGEL FLORES@ANGEL>
2018-12-12 22:35:14 -0500

    edicion del proyecto (3)

--- src/javaappgit/JavaAppGit.java
+++ src/javaappgit/JavaAppGit.java
@@ -18,6 +18,7 @@ public class JavaAppGit {
         // TODO code application logic here
         
         System.out.println("  asdadd");
+        System.out.println("  Hola mundo  ");
     }
     
 }
